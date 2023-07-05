861b748a5062733f3b952b3095fdaef95b52fac7
reza <mdaneshvar284@gmail.com>
2023-07-05 15:55:43 +0330

    added add complex api

---
+++ ComplexManagement/Dto/Complexes/AddComplexDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ComplexManagement.Dto.Complexes
+{
+    public class AddComplexDto
+    {
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [Range(4, 1000)]
+        public int UnitCount { get; set; }
+    }
+}
