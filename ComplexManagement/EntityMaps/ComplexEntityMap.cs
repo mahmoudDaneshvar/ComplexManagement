3dd5b39bd8280aad57197c8a43a0e1c733356221
reza <mdaneshvar284@gmail.com>
2023-07-05 15:21:42 +0330

    added entity maps

---
+++ ComplexManagement/EntityMaps/ComplexEntityMap.cs
@@ -0,0 +1,18 @@
+﻿using ComplexManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ComplexManagement.EntityMaps
+{
+    internal class ComplexEntityMap : IEntityTypeConfiguration<Complex>
+    {
+        public void Configure(EntityTypeBuilder<Complex> entity)
+        {
+            entity.ToTable("Complexes");
+            entity.HasKey(_ => _.Id);
+            entity.Property(_ => _.Id).ValueGeneratedOnAdd();
+            entity.Property(_ => _.Name).IsRequired().HasMaxLength(50);
+            entity.Property(_ => _.UnitCount).IsRequired();
+        }
+    }
+}
---
+++ ComplexManagement/EntityMaps/UnitEntityMap.cs
@@ -0,0 +1,19 @@
+﻿using ComplexManagement.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ComplexManagement.EntityMaps
+{
+    public class UnitEntityMap : IEntityTypeConfiguration<Unit>
+    {
+        public void Configure(EntityTypeBuilder<Unit> entity)
+        {
+            entity.HasKey(_ => _.Id);
+            entity.Property(x => x.Id).ValueGeneratedOnAdd();
+            entity.Property(_ => _.Name).IsRequired().HasMaxLength(50);
+
+
+            entity.Property(_ => _.BlockId).IsRequired();
+        }
+    }
+}
